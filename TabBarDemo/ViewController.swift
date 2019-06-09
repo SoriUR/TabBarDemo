ffa4c44fe12af97d86638b51122e4cf32e95cc0d
Юрий Сорокин <iurii.sorokin@touchin.ru>
2019-06-10 00:19:54 +0300

    Add custom tabBarItem

--- TabBarDemo/ViewController.swift
+++ TabBarDemo/ViewController.swift
@@ -1,8 +1,8 @@
 import UIKit
 import RAMAnimatedTabBarController
- 
-class CustomTabBar: RAMAnimatedTabBarController {
 
+class CustomTabBar: RAMAnimatedTabBarController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -10,20 +10,23 @@ class CustomTabBar: RAMAnimatedTabBarController {
         let yellow = YellowVC()
         let red = RedVC()
         
-        let item1 = RAMAnimatedTabBarItem(title: "Red", image: #imageLiteral(resourceName: "Image-2"), tag: 0)
+        let item1 = RAMAnimatedTabBarItem(title: "Green", image: #imageLiteral(resourceName: "Image-2"), tag: 0)
         item1.animation = RAMFlipLeftTransitionItemAnimations()
         
-        let item2 = RAMAnimatedTabBarItem(title: "Green", image: #imageLiteral(resourceName: "Image-1"), tag: 1)
+        let item2 = RAMAnimatedTabBarItem(title: "Yellow", image: #imageLiteral(resourceName: "Image-1"), tag: 1)
         item2.animation = RAMFlipTopTransitionItemAnimations()
         
-        let item3 = RAMAnimatedTabBarItem(title: "Yellow", image: #imageLiteral(resourceName: "Image-1"), tag: 2)
-        item3.animation = CustomAnimation()
+        let item3 = CustomTabBarItem()
         
         green.tabBarItem = item1
         yellow.tabBarItem = item2
         red.tabBarItem = item3
         
         viewControllers = [green, yellow, red]
+        
+        isBottomLineShow = true
+        bottomLineColor = .orange
+        bottomLineMoveDuration = 2
     }
 }
 
@@ -31,7 +34,7 @@ class GreenVC: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-    
+        
         view.backgroundColor = .green
     }
     
@@ -70,6 +73,10 @@ class RedVC: UIViewController {
 
 class CustomAnimation : RAMItemAnimation {
     
+    override func deselectedState(_ imageView: UIImageView, textLabel _: UILabel) {
+        imageView.transform = CGAffineTransform(scaleX: 0.5, y: 0.5)
+    }
+    
     override func playAnimation(_ icon: UIImageView, textLabel: UILabel) {
         textLabel.textColor = .red
         
@@ -82,11 +89,35 @@ class CustomAnimation : RAMItemAnimation {
     
     override func deselectAnimation(_ icon: UIImageView, textLabel: UILabel, defaultTextColor: UIColor, defaultIconColor: UIColor) {
         textLabel.textColor = .blue
-
+        
         icon.transform = CGAffineTransform(scaleX: 1, y: 1)
         
         UIView.animate(withDuration: 0.3) {
             icon.transform = CGAffineTransform(scaleX: 0.5, y: 0.5)
         }
+    }
+}
+
+class CustomTabBarItem: RAMAnimatedTabBarItem {
+    
+    override init() {
+        super.init()
+        
+        animation = CustomAnimation()
+        let label = UILabel()
+        label.text = "Red"
+        
+        let imageView = UIImageView()
+        imageView.image = #imageLiteral(resourceName: "Image-1")
+        
+        iconView = (icon: imageView, textLabel: label)
+        textFontSize = 25
+        
+        bgDefaultColor = .red
+        bgSelectedColor = .cyan
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
 }
